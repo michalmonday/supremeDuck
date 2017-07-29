56e33ac8c796a66a67dc28349ec2b9b25ee2bf08
Michal <michalmonday17@gmail.com>
2017-07-29 13:00:44 +0100

    Create btSerial.ino

---
+++ source/bluetooth customization/btSerial.ino
@@ -0,0 +1,44 @@
+/*  
+ *  The code below allows for communication between: the user <-> PC <-> Arduino <-> Bluetooth module.
+ *  The bluetooth module expects specific commands to be delivered to it that start with "AT".
+ *  The commands used for changing bluetooth device name and pin are the following:
+ *  AT+NAMEmyName
+ *  AT+PIN1234
+ * 
+ *  The 2 commands above would change the name to "myName" and pin to "1234". (the pin should be 4 digits long) 
+ *  After each command is submitted we should receive a response from the device, it should be saying: "OKsetname" and "OKsetpin"
+ *  To actually input these commands the code below has to be flashed, then the "Serial Monitor" has to be opened 
+ *  (at the right bottom corner of the serial monitor pick "No line ending" and "9600 baud")
+ *  Then input the initial testing command "AT", it should reply with "OK" which is always kind of satisfying to see :P
+ */
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 8); 
+//SoftwareSerial mySerial(8, 9);
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  } 
+  Serial.println("You can communicate between PC and arduino using serial"); // in this tutorial the communication will be used to set the name and pin of the bluetooth module
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+}
+
+void loop() { // run over and over
+
+  //read the bluetooth serial if it's available and write the content to the main USB serial (connected to PC and visible at "Tools -> Serial Monitor")
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+
+  //read the input of the user (from Serial Monitor) and pass it into the bluetooth which expects "AT" commands 
+  //the list of commands appropriate for the HC-06 bluetooth module is available at: http://www.micro4you.com/files/ElecFreaks/Bluetooth%20HC-06.pdf
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
